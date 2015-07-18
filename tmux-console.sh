cdfca79cb2d86cb7df66b2d5c1d2f4921d95a3b3
Maciej Sypień <maciej.sypien@gmail.com>
2015-07-18 15:34:55 +0200

    add tmux-console.sh to setup workspace

---
+++ tmux-console.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SESSION="pelican"
+FOLDER=$PWD    # to assign to a variable
+
+#create detached session named $SESSION
+tmux new-session -d -s $SESSION
+
+
+tmux new-session -d -s $SESSION
+tmux splitw -h -p 50 -t 0
+tmux splitw -v -p 50 -t 0
+
+tmux selectp -t 0
+tmux send-keys "cd $FOLDER && workon pelicanblog && pelican content --autoreload" 'C-m'
+
+tmux selectp -t 1
+tmux send-keys "cd $FOLDER" 'C-m'
+
+tmux selectp -t 2
+tmux send-keys "cd $FOLDER" 'C-m'
+
+tmux -2 attach-session -t $SESSION
