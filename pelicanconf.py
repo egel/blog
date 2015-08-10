aeef75acf4a05f3d3e30afcd461fc2367657c6ae
Maciej Sypień <maciej.sypien@gmail.com>
2015-08-10 09:53:56 +0200

    add configs

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,6 +15,7 @@ STATIC_PATHS        = ['images', 'downloads']
 ARTICLE_PATHS       = ['blog']
 PAGE_PATHS          = ['pages']
 
+# Formatting for URLs
 # ARTICLE_URL             = '{date:%Y}/{slug}.html'
 # ARTICLE_SAVE_AS         = '{date:%Y}/{slug}.html'
 # ARTICLE_LANG_SAVE_AS    = '{date:%Y}/{slug}-{lang}.html'
@@ -41,10 +42,8 @@ PAGE_PATHS          = ['pages']
 # YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
 # MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
 
-DEFAULT_METADATA = {
-  'status': 'draft',
-}
 
+DEFAULT_METADATA = {'status': 'draft',}
 DEFAULT_DATE_FORMAT = '%d %B %Y'
 
 THEME = "theme/egelance"
@@ -52,16 +51,18 @@ THEME = "theme/egelance"
 PLUGIN_PATHS = [ 'plugins' ]
 PLUGINS = [ 'tipue_search' ]
 
+MD_EXTENSIONS = ['codehilite(css_class=highlight)','extra']
+
 DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives', 'search'))
 
+DISQUS_SITENAME     = "The Egel's Blog"
+GOOGLE_ANALYTICS    = 'google-analytics-id'
 
-PYGMENTS_RST_OPTIONS = {
-    'classprefix': 'pgcss',
-    'linenos': 'table'
-}
+SUMMARY_MAX_LENGTH      = 50
+NEWEST_FIRST_ARCHIVES   = True  # Show most recent posts first
+REVERSE_CATEGORY_ORDER  = False
 
-
-SUMMARY_MAX_LENGTH = 50
+PYGMENTS_STYLE = "tango"
 
 # path-specific metadata
 # EXTRA_PATH_METADATA = {
@@ -70,15 +71,15 @@ SUMMARY_MAX_LENGTH = 50
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM           = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM      = None
+CATEGORY_FEED_ATOM      = 'feeds/category/%s.atom.xml'
+TAG_FEED_ATOM           = 'feeds/tag/%s.atom.xml'
 TRANSLATION_FEED_ATOM   = 'feeds/all-%s.atom.xml'
 AUTHOR_FEED_ATOM        = None
 AUTHOR_FEED_RSS         = None
 
-
-NEWEST_FIRST_ARCHIVES   = True
-REVERSE_CATEGORY_ORDER  = False
-
+FEEDS =  (('All posts', 'feeds/all.atom.xml'),
+          ('Category', 'feeds/category'),
+          ('OPW', 'feeds/tag/opw.atom.xml'),)
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
@@ -90,6 +91,11 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('Twitter', 'https://twitter.com/MaciejSypien'),
           ('Github', 'https://github.com/egel'),
           ('LinkedIn', 'https://www.linkedin.com/in/maciejsypien'),)
+
+HIDE_SIDEBAR = True
+ABOUT_ME = """
+Hey! I'm Maciej, geek amazed by computers, science, hacking, open source and astrophysics.
+"""
 
 DEFAULT_PAGINATION = 10
 
--- publishconf.py
+++ publishconf.py
@@ -10,15 +10,15 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
+SITEURL = 'http://pelican.egel.pl'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM       = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM  = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME     = "The Egel's Blog"
+GOOGLE_ANALYTICS    = 'UA-26456669-2'
