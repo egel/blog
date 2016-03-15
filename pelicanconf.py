e7baeceefdf0b41f6def67e16b66ab6046b76d31
Maciej Sypień <maciejsypien@gmail.com>
2016-03-15 19:29:08 +0100

    fix reading-list settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,8 +7,8 @@ AUTHOR          = 'Maciej Sypień'
 SITENAME        = "The Egel's Blog"
 SITE_SLOGAN     = "Do more in less time"
 SITE_SUBSLOGAN  = "Simplicity and clarity of the implementation"
-#SITEURL         = 'http://blog.dev'
-SITEURL         = 'http://pelican.egel.pl'
+SITEURL         = 'http://blog.dev'
+#SITEURL         = 'http://pelican.egel.pl'
 TIMEZONE        = 'Europe/Warsaw'
 DEFAULT_LANG    = 'en'
 CURRENT_DATE    = datetime.datetime.now()
@@ -128,7 +128,7 @@ PLUGINS = [  'related_posts', 'tag_cloud', 'goodreads_activity', 'simple_footnot
 
 MD_EXTENSIONS = [ 'codehilite(css_class=highlight, linenums=True)', 'extra'] # 'fenced_code',
 
-DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives', 'reading_list', 'search'))
+DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives', 'reading-list', 'search'))
 
 DISQUS_SITENAME     = "The Egel's Blog"
 GOOGLE_ANALYTICS    = 'UA-26456669-2'
