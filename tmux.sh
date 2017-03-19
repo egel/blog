3afb4e0642f641f2e64886284fa58352cb0c46c8
Maciej Sypień <maciej.sypien@atmina.de>
2017-03-19 16:04:06 +0100

    Update tmux script

--- tmux.sh
+++ tmux.sh
@@ -7,8 +7,8 @@ WORKON_ENV=pelicanblog
 
 #create detached session named $SESSION
 tmux new-session -d -s ${SESSION}
-tmux splitw -v -p 50 -t $((START_INDEX_NUMBER+0))
 tmux splitw -h -p 50 -t $((START_INDEX_NUMBER+0))
+tmux splitw -v -p 50 -t $((START_INDEX_NUMBER+0))
 
 tmux selectp -t $((START_INDEX_NUMBER+0))
 tmux send-keys "cd ${FOLDER} && workon pelicanblog && pelican content --autoreload" 'C-m'
@@ -18,6 +18,6 @@ tmux send-keys "cd ${FOLDER} && workon ${WORKON_ENV}" 'C-m'
 
 tmux selectp -t $((START_INDEX_NUMBER+2))
 tmux send-keys "cd ${FOLDER} && cd ../egelance-pelican-theme" 'C-m'
-tmux send-keys "gulp watch" 'C-m'
+tmux send-keys "yarn run start" 'C-m'
 
 tmux -2 attach-session -t ${SESSION}
